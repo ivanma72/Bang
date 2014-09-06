5eaf24bb03a5904df1c714eadc7cdc2e61ea1982
Ivan <ivanma72@gmail.com>
2014-09-06 11:27:02 -0400

    Added Michael's code Layout change

--- app/src/main/java/com/dev/mhacks/bang/ivanma/bang/BangActivity.java
+++ app/src/main/java/com/dev/mhacks/bang/ivanma/bang/BangActivity.java
@@ -5,104 +5,60 @@ import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
+import android.util.Log;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import java.util.ArrayList;
 import java.util.Random;
 
 
 public class BangActivity extends Activity {
 
-    private Button bangButton;
-    private TextView bangTextView;
+    ContentResolver cr;
+    ArrayList<Integer> contacts;
+    Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bang);
 
+        contacts = new ArrayList<Integer>();
 
-        bangTextView = (TextView)findViewById(R.id.contact_textview);
+        cr = getContentResolver();
+        cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
-        bangButton = (Button)findViewById(R.id.bang_button);
-        bangButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //find  random phone number from contacts
-                /*String pnumber = "9896986724";
-                String message = "Hello World";
-                try {
-                    PendingIntent sentPI = PendingIntent.getBroadcast(BangActivity.this, 0, new Intent("SMS_SENT"), 0);
-                    SmsManager.getDefault().sendTextMessage(pnumber, null, message, sentPI, null);
+        while (cursor.moveToNext()) {
+            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+
+            while (phones.moveToNext()) {
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                int type = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+                switch (type) {
+                    case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                        // do something with the Home number here...
+                        break;
+                    case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                        // do something with the Mobile number here...
+                        contacts.add(cursor.getPosition());
+                        break;
+                    case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                        // do something with the Work number here...
+                        break;
                 }
-                catch(Exception e){
-                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(BangActivity.this);
-                    AlertDialog dialog = alertDialogBuilder.create();
-                    dialog.setMessage(e.getMessage());
-                    dialog.show();
-
-                }*/
-
-                fetchContacts();
             }
-        });
+            phones.close();
+        }
     }
 
-    public void fetchContacts(){
-        String phoneNumber;
-        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
-        String _ID = ContactsContract.Contacts._ID;
-        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
-        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
-        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
-        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
-        StringBuffer output = new StringBuffer();
-        ContentResolver contentResolver = getContentResolver();
-        Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
-
-        //set up random contact row
-        int list_len = cursor.getCount();
-        long rand_seed = System.currentTimeMillis();
-        Random rnum = new Random(rand_seed);
-        int pos = rnum.nextInt(list_len);
-
-        //Get a random person + phone number
-        if(cursor.getCount() == 0){
-            return;
-        }
-
-        cursor.moveToPosition(pos);
-        String contact_id = cursor.getString(cursor.getColumnIndex(_ID));
-        String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
-        int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
-
-        while(hasPhoneNumber == 0){
-            pos = rnum.nextInt(list_len);
-            hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
-        }
-
-        output.append("\n First Name:" + name);
-        Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?",
-                new String[]{contact_id}, null);
-
-        if(phoneCursor.moveToNext()){
-            phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
-            output.append("\n Phone number: " + phoneNumber);
-        }
-        phoneCursor.close();
-
-        bangTextView.setText(output);
-        cursor.close();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -121,5 +77,52 @@ public class BangActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void bang(View view) {
+        Random r = new Random();
+        Integer idx = r.nextInt(contacts.size());
+
+        if (!cursor.moveToPosition(contacts.get(idx))) {
+            return;
+        }
+
+        String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+        Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+
+        while (phones.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            String number = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            number = number.replaceAll("[^\\d.]", "");
+            int type = phones.getInt(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+            switch (type) {
+                case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                    // do something with the Home number here...
+                    break;
+                case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                    // do something with the Mobile number here...
+                    sendMessage(name + " " + number, "2032463012");
+                    Log.i("BangActivity", "sendMessage called on number " + number);
+                    break;
+                case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                    // do something with the Work number here...
+                    break;
+            }
+        }
+        phones.close();
+    }
+
+    private void sendMessage(String message, String number) {
+        try {
+            PendingIntent pi = PendingIntent.getBroadcast(BangActivity.this, 0, new Intent("SMS_SENT"), 0);
+            SmsManager.getDefault().sendTextMessage(number,null, message, pi, null);
+            Log.i("BangActivity", "sendMessage end of try block");
+        } catch (Exception e) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(BangActivity.this);
+            AlertDialog dialog = alertDialogBuilder.create();
+            dialog.setMessage(e.getMessage());
+            dialog.show();
+        }
     }
 }
