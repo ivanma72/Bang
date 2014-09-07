0d7c7c08cd97ccbce91192167d8dc1d4db9f7990
Ivan <ivanma72@gmail.com>
2014-09-06 23:13:53 -0400

    Stable release: -Added info button -Changed app icon -View animations

--- app/src/main/java/com/dev/mhacks/bang/ivanma/bang/BangActivity.java
+++ app/src/main/java/com/dev/mhacks/bang/ivanma/bang/BangActivity.java
@@ -1,11 +1,11 @@
 package com.dev.mhacks.bang.ivanma.bang;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.Menu;
@@ -14,10 +14,11 @@ import android.view.View;
 import android.util.Log;
 import android.database.Cursor;
 import android.provider.ContactsContract;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
-import android.widget.TextSwitcher;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -25,6 +26,11 @@ import java.util.Random;
 
 
 public class BangActivity extends Activity {
+
+    private Button bangButton;
+    private ImageButton settingsButton;
+    private ImageButton infoButton;
+    private ImageButton shareButton;
 
     private TextView outputTxt;
 
@@ -38,7 +44,15 @@ public class BangActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bang);
 
+        //instantiate views
+        bangButton = (Button)findViewById(R.id.bang_button);
+        settingsButton = (ImageButton)findViewById(R.id.settings_button);
+        infoButton = (ImageButton)findViewById(R.id.info_button);
+        shareButton = (ImageButton)findViewById(R.id.share_button);
         outputTxt = (TextView)findViewById(R.id.outputTxt);
+
+        //do animation
+        fadeIn();
 
         puDB = new MySQLiteHelper(this);
 
@@ -144,6 +158,17 @@ public class BangActivity extends Activity {
             dialog.setMessage(e.getMessage());
             dialog.show();
         }
+    }
+
+    public void fadeIn(){
+
+        Animation fadein = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        bangButton.startAnimation(fadein);
+        settingsButton.startAnimation(fadein);
+        infoButton.startAnimation(fadein);
+        shareButton.startAnimation(fadein);
+
     }
 
     private String findMessage(){
